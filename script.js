e0d869d2c45eb9e4d977a2c2c5ce159e5811ab6d
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-27 10:53:17 +0530

    application complted

--- script.js
+++ script.js
@@ -42,6 +42,31 @@ async function fetchImage(count) {
                   <span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
+                      class="
+                        h-7
+                        w-7
+                        text-red-500
+                        hover:text-red-400
+                        transition
+                        duration-100
+                        cursor-pointer
+                      "
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  <span>${width}</span>
+                </div>
+                <div class="flex space-x-1">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
                       class="h-7 w-7 text-gray-600 cursor-pointer"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -55,58 +80,10 @@ async function fetchImage(count) {
                       />
                     </svg>
                   </span>
-                  <span>${width}</span>
-                </div>
-                <div class="flex space-x-1">
-                  <span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="
-                        h-7
-                        w-7
-                        text-red-500
-                        hover:text-red-400
-                        transition
-                        duration-100
-                        cursor-pointer
-                      "
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                  <span>${id}</span>
-                </div>
-                <div class="flex space-x-1">
-                  <span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="
-                        h-7
-                        w-7
-                        text-red-500
-                        hover:text-red-400
-                        transition
-                        duration-100
-                        cursor-pointer
-                      "
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </span>
                   <span>${height}</span>
                 </div>
+              
+             
               </div>
             </div>
           </div>`;
